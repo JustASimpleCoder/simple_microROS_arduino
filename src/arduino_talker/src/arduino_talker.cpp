b3ce29fa5ab4a376fe9903106a82be89841b5d8f
Jacob Cohen <thejacobcohen@gmail.com>
2024-11-22 17:45:48 -0500

    Fixed random num generator using rclcpp clock class and time. mixed CMake and paclage files to launch the nodes proerly

--- src/arduino_talker/src/arduino_talker.cpp
+++ src/arduino_talker/src/arduino_talker.cpp
@@ -11,15 +11,15 @@ using namespace std::chrono_literals;
 /* This example creates a subclass of Node and uses std::bind() to register a
 * member function as a callback from the timer. */
 
-class MinimalPublisher : public rclcpp::Node
+class LedColourPublisher : public rclcpp::Node
 {
   public:
-    MinimalPublisher()
-    : Node("minimal_publisher")
+    LedColourPublisher()
+    : Node("led_colour_publisher")
     {
       publisher_ = this->create_publisher<std_msgs::msg::String>("led_control", 10);
       timer_ = this->create_wall_timer(
-      500ms, std::bind(&MinimalPublisher::timer_callback, this));
+      500ms, std::bind(&LedColourPublisher::timer_callback, this));
     }
 
   private:
@@ -29,23 +29,27 @@ class MinimalPublisher : public rclcpp::Node
       message.data = "Randomly picking: " + std::to_string(random_pick()) ;
       RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
       publisher_->publish(message);
-      delay(1000);
+
+      rclcpp::sleep_for(std::chrono::nanoseconds(1000), nullptr);
     }
     rclcpp::TimerBase::SharedPtr timer_;
     rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
 
     int random_pick(){
         int pins[5] = {8,9,10,11,12};
-        auto time_now = ros::Time::now().toSec(); //get time with ros, isnteading of importing additional libraries
-        int index = std::chrono::duration_cast<std::chrono::seconds>(time_now.time_since_epoch()).count() % 5; // fancy chrono casting
-        return pin[index];
+        rclcpp::Clock clock(RCL_SYSTEM_TIME);
+        auto time_now = clock.now();//get time with ros, isnteading of importing additional libraries        
+        double secs = time_now.seconds();  
+        int index = static_cast<int>(secs) % 5; // fancy chrono casting
+        RCLCPP_INFO(this->get_logger(), "Selected pin: %d", pins[index]);
+        return pins[index];
     }
 };
 
 int main(int argc, char * argv[])
 {
   rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<MinimalPublisher>());
+  rclcpp::spin(std::make_shared<LedColourPublisher>());
   rclcpp::shutdown();
   return 0;
 }
